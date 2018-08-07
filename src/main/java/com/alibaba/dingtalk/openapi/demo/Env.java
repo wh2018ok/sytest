cae38fb7c6f196f8a3de1cc7b4e31e288ec5816a
Administrator <Administrator@1-PC>
2018-08-07 11:38:47 +0800

    bhn

---
+++ src/main/java/com/alibaba/dingtalk/openapi/demo/Env.java
@@ -0,0 +1,32 @@
+package com.alibaba.dingtalk.openapi.demo;
+
+
+/**
+ * 企业应用接入时的常量定义
+ */
+public class Env {
+
+    /**
+     * 企业应用接入秘钥相关
+     */
+    public static final String CORP_ID = "ding0d7a8a6b2065cda135c2f4657eb6378f";
+    public static final String CORP_SECRET = "NsE0C0Lpm9RqaOX9aOdYwa6_cpDMapPHld1cc5GCORP5Dnd1mFoxwXRTsN3dTTeI";
+    public static final String SSO_Secret = "FGEKU2NmrOjRTFR53SVWapz4kgb2AojKdG9sOX_KBXxRJRxLQJDfD4ReQlTioWBm";
+
+    /**
+     * DING API地址
+     */
+	public static final String OAPI_HOST = "https://oapi.dingtalk.com";
+    /**
+     * 企业应用后台地址，用户管理后台免登使用
+     */
+	public static final String OA_BACKGROUND_URL = "";
+
+
+    /**
+     * 企业通讯回调加密Token，注册事件回调接口时需要传递给钉钉服务器
+     */
+	public static final String TOKEN = "";
+	public static final String ENCODING_AES_KEY = "";
+	
+}
